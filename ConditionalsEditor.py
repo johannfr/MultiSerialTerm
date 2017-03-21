bf8e4a4156782828474d79c91a4621439c5fdff5
Johann Fridriksson <jof@jof.guru>
2017-03-21 21:30:02 +0100

    Fixed formatting for 80-cols

--- ConditionalsEditor.py
+++ ConditionalsEditor.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Tkinter import Tk, Frame, Button, Label, StringVar, Entry, Scrollbar, Text, Radiobutton, Checkbutton, PhotoImage, Listbox, END, N, S, E, W, VERTICAL, HORIZONTAL, YES, NO, TOP, FLAT, SUNKEN
+from Tkinter import Tk, Frame, Button, Label, StringVar, Entry, Scrollbar, \
+    Text, Radiobutton, Checkbutton, PhotoImage, Listbox, END, N, S, E, W,  \
+    VERTICAL, HORIZONTAL, YES, NO, TOP, FLAT, SUNKEN
 #from ttk import Treeview
 
 class TreeTest(Tk):
@@ -17,12 +19,22 @@ class TreeTest(Tk):
 
         up_down_button_frame = Frame(main_frame)
 
-        self.up_icon = PhotoImage(file="gtk-go-up.png")
-        up_button = Button(up_down_button_frame, text="Move up", image=self.up_icon, command=self.up_pressed)
+        self.up_icon = PhotoImage(file="images/gtk-go-up.png")
+        up_button = Button(
+            up_down_button_frame,
+            text="Move up",
+            image=self.up_icon,
+            command=self.up_pressed
+        )
         up_button.grid(column=0, row=0, sticky=(E))
 
-        self.down_icon = PhotoImage(file="gtk-go-down.png")
-        down_button = Button(up_down_button_frame, text="Move down", image=self.down_icon, command=self.down_pressed)
+        self.down_icon = PhotoImage(file="images/gtk-go-down.png")
+        down_button = Button(
+            up_down_button_frame,
+            text="Move down",
+            image=self.down_icon,
+            command=self.down_pressed
+        )
         down_button.grid(column=0, row=1, sticky=(E))
 
 
@@ -30,13 +42,37 @@ class TreeTest(Tk):
 
 
         condition_list = Frame(main_frame, relief=SUNKEN, borderwidth=1)
-        condition_list.grid(column=1, row=0, sticky=(N, S, E, W), padx=shared_pad_x, pady=shared_pad_y, columnspan=1)
+        condition_list.grid(
+            column=1,
+            row=0,
+            sticky=(N, S, E, W),
+            padx=shared_pad_x,
+            pady=shared_pad_y,
+            columnspan=1
+        )
         self.condition_list_scrollbar = Scrollbar(condition_list)
-        self.state_listbox = Listbox(condition_list, relief=FLAT, borderwidth=0, highlightthickness=0,  yscrollcommand=self.other_scroll)
+        self.state_listbox = Listbox(
+            condition_list,
+            relief=FLAT,
+            borderwidth=0,
+            highlightthickness=0,
+            yscrollcommand=self.other_scroll
+        )
         self.state_listbox.grid(column=0, row=0, padx=0)
-        self.condition_listbox = Listbox(condition_list, relief=FLAT, borderwidth=0, highlightthickness=0, yscrollcommand=self.other_scroll)
+        self.condition_listbox = Listbox(
+            condition_list,
+            relief=FLAT,
+            borderwidth=0,
+            highlightthickness=0,
+            yscrollcommand=self.other_scroll
+        )
         self.condition_listbox.grid(column=1, row=0, sticky=(E, W), padx=0)
-        self.execution_target_listbox = Listbox(condition_list, relief=FLAT, borderwidth=0, highlightthickness=0,  yscrollcommand=self.other_scroll)
+        self.execution_target_listbox = Listbox(
+            condition_list,
+            relief=FLAT,
+            borderwidth=0,
+            highlightthickness=0,yscrollcommand=self.other_scroll
+        )
         self.execution_target_listbox.grid(column=2, row=0, padx=0)
         self.condition_list_scrollbar.grid(column=3, row=0, sticky=(N, S))
         self.condition_list_scrollbar["command"] = self.condition_list_scroll
@@ -52,24 +88,56 @@ class TreeTest(Tk):
         if_label.grid(column=0, row=1, sticky=(N, E))
         if_text_variable = StringVar()
         if_entry = Entry(main_frame, textvariable=if_text_variable)
-        if_entry.grid(column=1, row=1, sticky=(E, W), padx=shared_pad_x, pady=shared_pad_y)
+        if_entry.grid(
+            column=1,
+            row=1,
+            sticky=(E, W),
+            padx=shared_pad_x,
+            pady=shared_pad_y
+        )
 
         then_label = Label(main_frame, text="Then:", padx=10)
         then_label.grid(column=0, row=2, sticky=(N, E))
         then_entry = Text(main_frame)
-        then_entry.grid(column=1, row=2, sticky=(N, S, E, W), padx=shared_pad_x, rowspan=2)
+        then_entry.grid(
+            column=1,
+            row=2,
+            sticky=(N, S, E, W),
+            padx=shared_pad_x,
+            rowspan=2
+        )
 
         option_frame = Frame(main_frame)
         execution_target_label = Label(option_frame, text="Execution target:")
-        execution_target_label.grid(column=0, row=0, sticky=(N, W), pady=(10, shared_pad_y))
+        execution_target_label.grid(
+            column=0,
+            row=0,
+            sticky=(N, W),
+            pady=(10, shared_pad_y)
+        )
         self.execution_target = StringVar()
         self.execution_target.set("debugger")
-        debugger_radiobutton = Radiobutton(option_frame, text="Debugger", variable=self.execution_target, value="debugger")
+        debugger_radiobutton = Radiobutton(
+            option_frame,
+            text="Debugger",
+            variable=self.execution_target,
+            value="debugger"
+        )
         debugger_radiobutton.grid(column=0, row=1, sticky=(N, W))
-        python_radiobutton = Radiobutton(option_frame, text="Python", variable=self.execution_target, value="python")
+        python_radiobutton = Radiobutton(
+            option_frame,
+            text="Python",
+            variable=self.execution_target,
+            value="python"
+        )
         python_radiobutton.grid(column=0, row=2, sticky=(N, W))
         state_label = Label(option_frame, text="State")
-        state_label.grid(column=0, row=3, sticky=(N, W), pady=(10, shared_pad_y))
+        state_label.grid(
+            column=0,
+            row=3,
+            sticky=(N, W),
+            pady=(10, shared_pad_y)
+        )
 
         active_checkbutton = Checkbutton(option_frame, text="Enabled")
         active_checkbutton.grid(column=0, row=4, sticky=(N, W))
@@ -82,15 +150,26 @@ class TreeTest(Tk):
         update_button.grid(column=1, row=0, sticky=(E))
         delete_button = Button(button_frame, text="Delete")
         delete_button.grid(column=2, row=0, sticky=(E))
-        button_frame.grid(column=0, row=4, columnspan=2, sticky=(E), padx=shared_pad_x, pady=shared_pad_y)
+        button_frame.grid(
+            column=0,
+            row=4,
+            columnspan=2,
+            sticky=(E),
+            padx=shared_pad_x,
+            pady=shared_pad_y
+        )
 
         close_frame = Frame(main_frame, bg="pink")
         close_button = Button(close_frame, text="Close")
         close_button.grid(column=0, row=0, sticky=(S, E))
-        close_frame.grid(column=0, row=5, columnspan=2, sticky=(S, E), padx=shared_pad_x, pady=(15, shared_pad_y))
-
-
-
+        close_frame.grid(
+            column=0,
+            row=5,
+            columnspan=2,
+            sticky=(S, E),
+            padx=shared_pad_x,
+            pady=(15, shared_pad_y)
+        )
 
 
         self.grid_columnconfigure(0, weight=1)
